298b3f38f0c3c0138d4e3dab2d4a274389a1ea95
Swasti3719 <swastiprakashbhanja3719@gmail.com>
2024-02-02 18:44:50 +0530

    Your commit message here

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,22 @@
+export function VideoCard(props : any){
+    return <div>
+        <img className = "rounded-xl" src = {props.image}></img> 
+        <div className = "grid grid-cols-12">
+            <div className = "col-span-1">
+                <img  className = {"rounded-full w-20 h-18"} src = {props.thumbImage}></img>
+            </div>
+            <div className = "col-span-11 pl-3">
+                <div>
+                    {props.title}
+                </div> 
+                <div className = "text-gray-400 text-xs">
+                    {props.channelName}  
+                </div>
+                <div className = "text-gray-400 text-xs">
+                    {props.views} 
+                </div>
+            </div>
+        </div>
+        <div className = ""></div>
+    </div>
+} 
---
+++ src/components/VideoGrid.tsx
@@ -0,0 +1,27 @@
+// import { VideoCard } from "./VideoCard";
+const Video = [{
+    image : "photo1.jpg" ,
+    thumbImage : "photo2.jpg" , 
+    title : "Bangladesh V India | Match Highlights | U19 CWC 2024" ,
+    channelName : "ICC" , 
+    views : "3.8M views . 4 days ago "
+} , 
+{
+    image : "photo1.jpg" ,
+    thumbImage : "photo2.jpg" , 
+    title : "Bangladesh V India | Match Highlights | U19 CWC 2024" ,
+    channelName : "ICC" , 
+    views : "3.8M views . 4 days ago "
+},
+{
+    image : "photo1.jpg" ,
+    thumbImage : "photo2.jpg" , 
+    title : "Bangladesh V India | Match Highlights | U19 CWC 2024" ,
+    channelName : "ICC" , 
+    views : "3.8M views . 4 days ago "
+}]
+export const Videos = Video.map(temp=>{
+    <div>
+        
+    </div>
+})
